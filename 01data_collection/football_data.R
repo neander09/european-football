762b1ed7227debe84b7300cae2d10a6b00f44972
neander09 <18016834+neander09@users.noreply.github.com>
2019-10-15 22:35:41 +0200

    Add files via upload

--- 01data_collection/football_data.R
+++ 01data_collection/football_data.R
@@ -14,9 +14,11 @@ library(tidyverse)
 # function to find links to the csv files on webpages
 find_csv_links <- function(url) {
   page <- read_html(url)
-  csv_links <- html_nodes(page, "a") 
-  csv_links <- html_attr(csv_links, "href") 
-  csv_links <- str_subset(csv_links, "^.*\\.csv$")
+  csv_nodes <- html_nodes(page, "a[href$='.csv']")
+  csv_links <- html_attr(csv_nodes, "href")
+  csv_names <- html_text(csv_nodes)
+  csv_div <- sub(".*/.*/", "", csv_links) 
+  names(csv_links) <- csv_names
   return(csv_links)
 }
 
@@ -30,9 +32,9 @@ extract_season <- function(string) {
   }
 }
 
-# function that generates the part of a "CREATE TABLE"-command in which the 
-# column names and corresponding data types are specified based on the column 
-# names and types of a dateframe. input: dataframe, output: string
+# function that generates the part of a "CREATE TABLE"-sqlite-command in which  
+# the column names and corresponding data types are specified based on the  
+# column names and types of a dateframe. input: dataframe, output: string
 col_names_types <- function(df) {
   field_types <- unlist(sapply(df, class))
   field_names <- names(field_types)
@@ -43,6 +45,19 @@ col_names_types <- function(df) {
   part_of_query <- paste(field_types, collapse = ", ")
   return(part_of_query)
 }
+
+# function that returns information about the countries and their corresponding
+# football leagues that is needed for the "leagues"-table 
+get_league_names <- function(url) {
+  page <- read_html(url)
+  csv_nodes <- html_nodes(page, "a[href$='.csv']")
+  csv_links <- html_attr(csv_nodes, "href")
+  csv_names <- html_text(csv_nodes)
+  csv_div <- sub(".csv", "", sub(".*/.*/", "", csv_links)) 
+  country_leagues <- unique(paste(csv_names, csv_div, sep = "_"))
+  return(country_leagues)
+}
+
 
 # start of the script
 page <- read_html("https://www.football-data.co.uk/data.php")
@@ -57,10 +72,20 @@ df <- cbind(df, tbl_lnks)
 key_string <- " Football Results"
 df$country <- sub(key_string, "", df[, 2])
 df$url <- paste0("https://football-data.co.uk/", df[, 4])
-countries <- setNames(as.list(df$url), df$country)
+countries <- setNames(df$url, df$country)
+# create dataframe "leagues"
+leagues <- sapply(countries, get_league_names)
+leagues <- unlist(leagues)
+names(leagues) <- sub("\\d", "", names(leagues))
+leagues <- data.frame(names(leagues), leagues)
+leagues$fd_league <- sub(".*_", "", leagues$leagues)
+leagues$leagues <- sub("_.*", "", leagues$leagues)
+leagues <- leagues %>% rename(country = names.leagues., league = leagues)
+leagues <- leagues %>% 
+  filter(!(country == "England" & grepl("^Division.*$", league)))
 # find all csv files on the pages
 csv_links <- sapply(countries, find_csv_links)
-csv_links <- sapply(csv_links, unique)
+csv_links <- sapply(csv_links, function(x) x[!duplicated(x)])
 csv_links <- sapply(
   csv_links, function(x) paste0("https://www.football-data.co.uk/", x)
 )
@@ -116,8 +141,6 @@ main_leagues <- select(main_leagues, country, season, Div:AFKC)
 # change "Date" to nice date
 main_leagues$Date <- dmy(main_leagues$Date)
 main_leagues$season <- as.integer(main_leagues$season)
-
-leagues <- read_csv("leagues.csv")
 
 # create sqlite database
 conn <- dbConnect(SQLite(), "football.db")
@@ -206,3 +229,4 @@ dbSendQuery(conn,
   )
 )
 dbWriteTable(conn, "games", main_leagues, append = TRUE)
+rm(list = ls())
